1701d53fb931e105663d4e11253fed5d794e1a48
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-12-07 13:06:10 +0200

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+f = int(open('INPUT.txt', 'r').read())
+string = ''
+sumAllNumbers = 0
+if 0 < f <= 10**4:
+    for x in range(1, f + 1):
+        sumAllNumbers += x
+
+if sumAllNumbers > 0:
+    string = str(sumAllNumbers)
+
+the_file = open('OUTPUT.txt', 'w')
+the_file.write(string)
