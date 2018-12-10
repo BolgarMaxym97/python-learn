534e026e9b4bdd64c11685e7c82bb6b48db4cf21
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-12-10 22:07:44 +0200

    the smallest diff

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-f = open('INPUT.txt', 'r')
-arr = [x.strip() for x in f.readlines()]
-threeCount = 0
-fourCount = 0
-three = [item for item in arr[1].split() if int(item) % 2 != 0]
-four = [item for item in arr[1].split() if int(item) % 2 == 0]
+def diffArr(number):
+    allDiffs = []
+    diffNumber = 1
+    while diffNumber <= number:
+        if number % diffNumber == 0:
+            allDiffs.append(diffNumber)
+        diffNumber += 1
+    return allDiffs
 
-file = open('OUTPUT.txt', 'w')
-file.write(' '.join(three) + '\n')
-file.write(' '.join(four) + '\n')
-file.write('YES' if len(four) > len(three) else 'NO')
+f = int(open('input.txt', 'r').read())
+allDiffs = diffArr(f)
+diffNumbersSum = {}
+for item in allDiffs:
+    itemSumm = 0
+    for strItem in str(item):
+        itemSumm += int(strItem)
+    diffNumbersSum[int(item)] = itemSumm
+
+maxWithoutCheck = max(diffNumbersSum.values())
+minValue = 10**5
+for key, value in diffNumbersSum.items():
+    if value == maxWithoutCheck and key < minValue:
+        minValue = key
+
+w = open('output.txt', 'w').write(str(minValue))
